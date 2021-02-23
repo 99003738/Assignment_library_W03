7d38aa88bf93cb799fadb1e1bd1fe7c8f467c174
99003738 <abhishek.mishra1@ltts.com>
2021-02-24 00:08:43 +0530

    changing few line for printf.

--- test/test.c
+++ test/test.c
@@ -8,9 +8,15 @@ int main()
 {
     
     char ch1[10] = "what", ch2[10] = "is this";
-    int *newstring=NULL;
+    int* newstring;
     mystrcat(ch1,ch2,newstring);
-    
+    printf("I am working : \n");
+    char ch = *newstring;
+    while(ch != '\0')
+    {
+        printf("%c",ch);
+        newstring++;
+    }
     
     return 0;
 }
