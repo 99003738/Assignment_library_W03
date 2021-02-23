2b79e3fbf2418896809c500c5ca2e3f6aa54eb3a
99003738 <abhishek.mishra1@ltts.com>
2021-02-23 18:53:22 +0530

    saving all the changes.

--- test/test.c
+++ test/test.c
@@ -9,9 +9,12 @@ int main()
     
     char ch1[10] = "what", ch2[10] = "is this";
     mystrcat(ch1,ch2);
-    while(*newstring != '/0')
+    char ch = newstring[0];
+    while(ch != '/0')
     {
+    
     printf(" %c", newstring);
+    ch = *newstring++;
     }
     return 0;
 }
