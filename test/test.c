17ee095fdbb8fced387cfb11273a3ed87bab4db8
99003738 <abhishek.mishra1@ltts.com>
2021-02-23 23:56:42 +0530

    solving the error for execution.

--- test/test.c
+++ test/test.c
@@ -7,13 +7,10 @@
 int main()
 {
     
-    char ch1[10] = "what", ch2[10] = "is this", *newstring;
+    char ch1[10] = "what", ch2[10] = "is this";
+    int *newstring=NULL;
     mystrcat(ch1,ch2,newstring);
-    char ch = newstring[0];
-    while(ch != '\0')
-    {
-    printf(" %c", newstring);
-    ch = *newstring++;
-    }
+    
+    
     return 0;
 }
