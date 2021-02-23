a8553ddfa19c10114c62b11eee910158e493b458
99003738 <abhishek.mishra1@ltts.com>
2021-02-23 15:06:26 +0530

    written code for test in test file.

--- src/string.c
+++ src/string.c
@@ -1,12 +1,20 @@
 #include "mystring.h"
 
-int mystrlen(char *string)
+int mystrlen(char *string[size])
 {
-
-
-
-
-
+    //printf("Enter your string :")
+    //chptr = (char *)malloc(size*sizeof(char));
+     char ch = string[0];
+     int count = 0;
+     while(ch != '\0')
+     {
+         if (ch == ' ')
+         {
+             count = count+1; 
+         }
+         ch = [string++];
+     }
+return count;
 }
 
 
--- test/test.c
+++ test/test.c
@@ -8,7 +8,7 @@ int main()
 {
     int count;
     count = mystrlen(what is your name);
-
+     printf(" %d",count);
 
     return 0;
 }
