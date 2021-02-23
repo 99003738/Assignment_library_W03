96b3f2b6edc56209d86ec510c699fa2d9b81e3fc
99003738 <abhishek.mishra1@ltts.com>
2021-02-24 00:08:04 +0530

    no error but output is not as desired.

--- include/mystring.h
+++ include/mystring.h
@@ -14,7 +14,7 @@ int mystrlen(char string[size]);
 char new_string[size];
 
 int mystrcpy(char *source, char *destination);
-int mystrcat(char *string_1, char *string_2, char *newstring);
+int mystrcat(char* string_1, char* string_2, int* newstring);
 int mystrcmp();
 
 
