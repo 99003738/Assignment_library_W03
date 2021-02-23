a7cd5a727bca6b435698c698cc5121baf6794621
99003738 <abhishek.mishra1@ltts.com>
2021-02-23 23:49:20 +0530

    modified some variable name to remove error.

--- test/test.c
+++ test/test.c
@@ -8,11 +8,10 @@ int main()
 {
     
     char ch1[10] = "what", ch2[10] = "is this", *newstring;
-    mystrcat(ch1,ch2,&newstring);
+    mystrcat(ch1,ch2,newstring);
     char ch = newstring[0];
-    while(ch != '/0')
+    while(ch != '\0')
     {
-    
     printf(" %c", newstring);
     ch = *newstring++;
     }
