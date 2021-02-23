bb8e28b1674f41da6f5aac4efcdcb5e876e15053
99003738 <abhishek.mishra1@ltts.com>
2021-02-23 18:51:09 +0530

    modified the test.c

--- test/test.c
+++ test/test.c
@@ -9,7 +9,9 @@ int main()
     
     char ch1[10] = "what", ch2[10] = "is this";
     mystrcat(ch1,ch2);
-    
-
+    while(*newstring != '/0')
+    {
+    printf(" %c", newstring);
+    }
     return 0;
 }
