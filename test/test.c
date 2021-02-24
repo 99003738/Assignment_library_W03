f7439d81a7aaf288ed3723f1cddbbef3fcd7ce96
99003738 <abhishek.mishra1@ltts.com>
2021-02-24 06:23:39 +0530

    test file fmodified.

--- test/test.c
+++ test/test.c
@@ -7,14 +7,17 @@
 int main()
 {
     
-    char ch1[10] = "what", ch2[10] = "is this";
+    /*char ch1[10] = "what", ch2[10] = "is this";
   
     char* ch = (mystrcat(ch1,ch2));
     printf("\nI am working :");
         while( *ch != '\0'){
       printf("%c",*ch);
             *ch++;
-        }
+        }*/
     
+int i =0;
+
+
     return 0;
 }
