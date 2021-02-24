9dea53f0933980fd699709878eea735a73639810
99003738 <abhishek.mishra1@ltts.com>
2021-02-24 06:23:18 +0530

    defininition for fatorial ,odified.

--- src/mathfun.c
+++ src/mathfun.c
@@ -1,7 +1,28 @@
  #include "myutils.h"
 
 
- int factorial()
- {
-     
- }
+error_t find_factorial(int num, long* result)
+{
+   // long temp_result = 1;
+    if(1 == num)
+    {
+        return num;
+    }
+    else if (0 > num)
+    {
+        return NEGATIVE_NUMBER;
+    }
+    else if (0 == num)
+    {
+        return ZERO;
+    }
+    else
+    {
+        while ( 1 <= num )
+        {
+            *result *= num--;
+        }
+        return SUCCESS;
+    }
+    
+}
